a91abf4b583456c705bec5e7d74433b7f16e615e
Miguel4577 <miguelmahos2@gmail.com>
2025-05-12 12:01:16 -0500

    Pruebas Y actualizacion BlackJack

--- src/model/salaDeJuego/juego/juegosDeCartas/BlackJack.py
+++ src/model/salaDeJuego/juego/juegosDeCartas/BlackJack.py
@@ -21,17 +21,30 @@ class BlackJack(JuegoDeCartas):
         print("falta implementar pedir()")
         pass
     
-    def plantarse(self, mano_jugador,plantarse: str):
-        
-        if plantarse[0] == "S" or plantarse[0] == "s":
-            return print("El jugador se plantó.")
-        elif plantarse[0] == "N" or plantarse[0] == "n":            
-            mano_jugador.append(self.repartir_cartas())
+    def ganador(self, mano_jugador, mano_crupier):
+        puntos_jugador = self.calcular_puntos(mano_jugador)
+        puntos_crupier = self.calcular_puntos(mano_crupier)
+        if puntos_jugador > 21:
+            print("El jugador ha perdido, se ha pasado de 21 puntos.")
+            print(f"Mano del crupier: {mano_crupier}  || Puntos: {puntos_crupier}")
+        elif puntos_crupier > 21:
+            print("El crupier ha perdido, se ha pasado de 21 puntos.")
+        elif puntos_jugador > puntos_crupier:
+            print("El jugador ha ganado.")
+            print(f"Mano del crupier: {mano_crupier}  || Puntos: {puntos_crupier}")
+
+        elif puntos_jugador < puntos_crupier or puntos_crupier == 21:
+            print("El crupier ha ganado.")
+            print(f"Mano del crupier: {mano_crupier}  || Puntos: {puntos_crupier}")
+
     
     def calcular_puntos(self, mano) -> int:
 
-        #Calcula los puntos de la mano del jugador y del crupier.
-        puntos = sum([BlackJack._cartas[carta] for carta in mano])
+        #Calcula los puntos de la mano dada
+        puntos=sum(BlackJack._cartas[carta] for carta in mano)
+        # Si hay un As y los puntos son mayores a 21, resta 10 puntos
+        if 'A' in mano and puntos > 21:
+            puntos -= 10
         return puntos
     
     def doblar(self, jugador: Usuario):
@@ -46,26 +59,22 @@ class BlackJack(JuegoDeCartas):
         print("falta implementar retirarse()")
         pass
     
-    def repartir_cartas(self): 
+    def repartir_cartas(self) -> str: 
         #Reparte una carta al jugador o al crupier y devuelve el valor de la carta.
-        random.choice(list(BlackJack._cartas.keys()))
+        return random.choice(list(BlackJack._cartas.keys()))
+        
 
     def cartasIniciales(self):
         #Reparte dos cartas al jugador y devuelve el valor de las cartas.
-        mano_jugador = [self.repartir_cartas(), self.repartir_cartas()]
-        mano_crupier = [self.repartir_cartas(), self.repartir_cartas()]
-        return mano_jugador, mano_crupier
+        mano_inicial = [self.repartir_cartas(), self.repartir_cartas()]
+        
+        return mano_inicial
 
     def mostrar_CartasYPuntos(self, mano_jugador, mano_crupier):
         #Muestra las cartas de la mano.
-        print(f"Mano del jugador: {mano_jugador}")
-        print(f"Mano del crupier: {mano_crupier}")
+        print(f"Mano del jugador: {mano_jugador}  || Puntos: {self.calcular_puntos(mano_jugador)}")
+        print(f"Mano visible del crupier: {mano_crupier[0]}")
         #Muestra los puntos de el jugador y de crupier.
-        puntos_crupier = self.calcular_puntos(mano_crupier)
-        puntos_jugador = self.calcular_puntos(mano_jugador)
-
-        print(f"Puntos del jugador: {puntos_jugador}")
-        print(f"Puntos del crupier: {puntos_crupier}")
 
     def apostar(self, jugador: Usuario, monto: float):
         print("falta implementar apostar()")
@@ -74,14 +83,34 @@ class BlackJack(JuegoDeCartas):
     def inicializar_juego(self):
         """Inicializa el juego de BlackJack."""
         # Reparte las cartas iniciales al jugador y al crupier
-        mano_jugador, mano_crupier = self.cartasIniciales()
+        mano_jugador = self.cartasIniciales()
+        mano_crupier = self.cartasIniciales()
+        puntos =0
 
-        while self.calcular_puntos(mano_jugador) < 21:
+        while  puntos < 21 :
+            puntos=self.calcular_puntos(mano_jugador)
             # Muestra las cartas y los puntos 
             self.mostrar_CartasYPuntos(mano_jugador, mano_crupier)
             # Pregunta al jugador si quiere pedir otra carta o plantarse
-            self.plantarse(mano_jugador, "si")
-            # Muestra las cartas y los puntos 
+            plantarse = str(input("¿Quieres plantarte? (S/N): ").lower())
+            # Si el jugador quiere pedir otra carta, se le reparte una nueva carta
+            if plantarse == "n":
+                mano_jugador.append(self.repartir_cartas())
+            elif plantarse == "s":
+                self._plantarse = True
+                break
+        # Si el jugador se planta, es hora de jugar al crupier
+        puntos=self.calcular_puntos(mano_crupier)
+        if puntos < 17:
+            # El crupier pide cartas hasta que tenga al menos 17 puntos
+            while puntos < 17:
+                mano_crupier.append(self.repartir_cartas())
+                puntos = self.calcular_puntos(mano_crupier)
+            #Si el crupier tiene mas de 17 puntos muestra las cartas y los puntos del crupier
+            if puntos >  17:
+                print(f"Mano del crupier: {mano_crupier}  || Puntos: {puntos}")
+        # Determina el ganador
+        self.ganador(mano_jugador, mano_crupier)    
 
 
     
